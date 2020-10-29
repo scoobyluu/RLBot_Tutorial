129d539af8adbf840736b15b554692f4c614622a
scoobyluu <stevenluu123@gmail.com>
2020-10-29 12:45:12 -0700

    migrating architecture

---
+++ CSharpBot/Bot/DecisionMaker.cs
@@ -0,0 +1,25 @@
+﻿using Bot.Skills;
+using Bot.Utilities.Processed.Packet;
+
+namespace Bot
+{
+    internal class DecisionMaker
+    {
+        private readonly SkillManager _skillManager;
+
+        public DecisionMaker(SkillManager skillManager)
+        {
+            _skillManager = skillManager;
+
+            // TODO: Test a skill here.
+            _skillManager.SetSkill(new TestSkill());
+        }
+
+        internal void Tick(Packet packet)
+        {
+            // TODO: Custom decision-making logic will go here.
+
+            _skillManager.Tick(packet);
+        }
+    }
+}
