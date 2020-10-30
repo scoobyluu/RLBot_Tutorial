16c5d7a87c7c5444bbcbc22c232c6412627d237a
scoobyluu <stevenluu123@gmail.com>
2020-10-29 20:42:15 -0700

    creating starter project

---
+++ startingProject/CSharpBot/Bot/Skills/ExampleSkill.cs
@@ -0,0 +1,27 @@
+﻿using Bot.Utilities.Processed.Packet;
+using RLBotDotNet;
+
+namespace Bot.Skills
+{
+    internal class EmptySkill : ISkill
+    {
+        public bool IsFinished => false;
+
+        public void Finish()
+        {
+        }
+
+        public void Init()
+        {
+        }
+
+        public void Interrupt()
+        {
+        }
+
+        public Controller Tick(Packet packet)
+        {
+            return new Controller();
+        }
+    }
+}
